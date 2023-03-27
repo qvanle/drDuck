91d4c31870325bdf074c92e92509f046bf1d9d02
qvanle <vanle199tk@gmail.com>
2023-03-28 00:12:36 +0700

    setup environment

---
+++ makefile
@@ -0,0 +1,18 @@
+all: clearscreen run
+	
+.PHONY: all run
+run: BUILD 
+	build/ShowMePls
+
+BUILD:
+	cmake -B build/
+	make -C build
+
+DEBUG:
+	cmake -DCMAKE_BUILD_TYPE=DEBUG -B build/ 
+	make -C build
+
+.PHONY: clearscreen
+clearscreen:
+	clear
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,56 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_quit.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_events.h>
+
+bool status = 1;
+SDL_Window* window;
+int WIDTH = 960;
+int HEIGHT = 540;
+
+void init()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow(
+        "Dr Duck", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        WIDTH, 
+        HEIGHT, 
+        SDL_WINDOW_SHOWN
+   );
+}
+
+void Action()
+{
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            status = 0;
+        }
+    }
+}
+
+void shutdown()
+{
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    status = 0;
+    WIDTH = 0;
+    HEIGHT = 0;
+}
 
 int main()
 {
+    init();
+    while(status)
+        Action();
+
+    shutdown();
 
     return 0;
 }
