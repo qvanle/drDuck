be7851805d577f6172708923cd6071bc38d4f8ec
qvanle <vanle199tk@gmail.com>
2023-03-28 02:28:42 +0700

    add system library

---
+++ include/SYSTEM.hpp
@@ -0,0 +1,15 @@
+#ifndef SYSTEM
+#define SYSTEM
+
+#include <cstring>
+
+namespace GLOBAL
+{
+    extern const char* BackgroundFolder;
+    extern const char* ButtonFolder;
+    extern const char* AttributeFolder;
+}
+
+char *& combineLink(const char *dir, const char *name);
+
+#endif
