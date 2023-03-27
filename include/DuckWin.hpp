78b440b771643c858e4f5109c1deeb421cd52877
qvanle <vanle199tk@gmail.com>
2023-03-28 00:40:47 +0700

    remove cache

---
+++ include/DuckWin.hpp
@@ -0,0 +1,29 @@
+#ifndef WINDOW
+#define WINDOW
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_quit.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_events.h>
+
+class MyWindow
+{
+private:
+    int status;
+    int WIDTH;
+    int HEIGHT;
+
+    SDL_Window* window;
+
+    void shutdown();
+
+public:
+    void init();
+    void action();
+    bool isOpen();
+    bool isClose();
+    ~MyWindow();
+};
+
+#endif
---
+++ src/DuckWin.cpp
@@ -0,0 +1,54 @@
+#include <DuckWin.hpp>
+
+void MyWindow::init()
+{
+    status = 1;
+    WIDTH = 960;
+    HEIGHT = 540;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow(
+        "Dr Duck",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+}
+
+void MyWindow::action()
+{
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            status = 0;
+            shutdown();
+        }
+    }
+}
+
+void MyWindow::shutdown()
+{
+    if(window == nullptr) return ;
+    SDL_DestroyWindow(window);
+    window = nullptr;
+
+    SDL_Quit();
+    status = 0;
+    WIDTH = 0;
+    HEIGHT = 0;
+} 
+
+bool MyWindow::isOpen()
+{
+    return status == 1;
+}
+
+bool MyWindow::isClose()
+{
+    return status == 0;
+}
