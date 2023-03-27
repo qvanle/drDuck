0904670e4de10a211509d34b70d78371a6c9fb41
qvanle <vanle199tk@gmail.com>
2023-03-28 02:20:40 +0700

    small change

--- include/Display.hpp
+++ include/Display.hpp
@@ -1,12 +1,17 @@
 #ifndef DISPLAY
 #define DISPLAY
 
+#include <SDL2/SDL.h>
+#include <SDL_surface.h>
+
 class Display
 {
 private:
-
+    SDL_Texture* background;
+    SDL_Renderer *& renderer;
 public:
-
+    void init(const char* dir, const char* name);
+    void setRenderer(SDL_Renderer *& ren);
 };
 
 #endif
