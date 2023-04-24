63b94c4ef97000793a75264cc35ea008404e0d16
qvanle <vanle199tk@gmail.com>
2023-04-24 20:29:53 +0700

    update asset and add code demo for static array insert

--- include/Script.hpp
+++ include/Script.hpp
@@ -4,20 +4,21 @@
 #include <SYSTEM.hpp>
 #include <Sketch.hpp>
 #include <SDL2/SDL.h>
+#include <Object.hpp>
 
 
 class Script : public Sketch
 {
 private:
-    std::vector<Sketch*> texts;
-    json Data;
-    int dx, dy;
+    std::vector<Sketch*> lines;
+    Object* obj;
     SDL_Renderer* ren;
 public: 
     Script();
     ~Script();
 
-    void loadTexts(std::string s);
+    void loadObject(const json& mem);
+    void loadHighlight(const json & mem);
     void init(const json& mem);
     void highlightLine(int k);
     void unHighlighLine(int k);
